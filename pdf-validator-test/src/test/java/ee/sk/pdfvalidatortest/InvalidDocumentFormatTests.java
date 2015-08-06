490ee1e19d583f7a6952629e8851db21a4f454d6
Madis <madispiigli@gmail.com>
2015-08-06 14:16:09 +0300

    add new tests

--- pdf-validator-test/src/test/java/ee/sk/pdfvalidatortest/InvalidDocumentFormatTests.java
+++ pdf-validator-test/src/test/java/ee/sk/pdfvalidatortest/InvalidDocumentFormatTests.java
@@ -1,11 +1,11 @@
 package ee.sk.pdfvalidatortest;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.w3c.dom.Document;
 
 import java.util.Collections;
 
-import org.junit.Test;
-import org.w3c.dom.Document;
+import static org.junit.Assert.assertEquals;
 
 public class InvalidDocumentFormatTests extends PdfValidatorSoapTests {
 	
@@ -13,43 +13,57 @@ public class InvalidDocumentFormatTests extends PdfValidatorSoapTests {
     public void PAdESDocumentShouldPass() {
         String httpBody = post(validationRequestFor(readFile("Signature-P-EE_AS-7.pdf"))).
                 andReturn().body().asString();
-        
         assertEquals("PAdES_BASELINE_LT", signatureFormat(simpleReport(httpBody)));
     }
-	
+
 	@Test
     public void ASiCDocumentShouldFail() {
         String httpBody = post(validationRequestFor(readFile("Signature-A-EE-1.asice"))).
                 andReturn().body().asString();
-        
+
         assertEquals("Document format not recognized/handled", soapFaultstring(httpBody));
     }
-	
+
 	@Test
     public void XAdESDocumentShouldFail() {
         String httpBody = post(validationRequestFor(readFile("Signature-X-AT-1.xml"))).
                 andReturn().body().asString();
-        
+
         assertEquals("Document format not recognized/handled", soapFaultstring(httpBody));
     }
-	
+
 	@Test
     public void CAdESDocumentShouldFail() {
         String httpBody = post(validationRequestFor(readFile("Signature-C-AT-1.p7"))).
                 andReturn().body().asString();
-        
+
         assertEquals("Document format not recognized/handled", soapFaultstring(httpBody));
     }
-	
+    @Test
+    public void ZipDocumentShouldFail() {
+        String httpBody = post(validationRequestFor(readFile("42.zip"))).
+                andReturn().body().asString();
+
+        assertEquals("Document format not recognized/handled", soapFaultstring(httpBody));
+    }
+
+    @Test
+    public void DocDocumentShouldFail() {
+        String httpBody = post(validationRequestFor(readFile("Signature-P-EE_AS-1.doc"))).
+                andReturn().body().asString();
+        assertEquals("Document format not recognized/handled", soapFaultstring(httpBody));
+    }
+
+
 	private byte[] readFile(String fileName) {
     	return readFileFromTestResources("invalid_format_documents/", fileName);
     }
-	
+
 	private String signatureFormat(Document simpleReport) {
         String stringResult = XmlUtil.findElementByXPath(
                 simpleReport,
                 "//d:SimpleReport/d:Signature",
-                Collections.singletonMap("d", "http://dss.esig.europa.eu/validation/diagnostic")).getAttribute("SignatureFormat");
+                Collections.singletonMap("d", "http://dss.markt.ec.europa.eu/validation/diagnostic")).getAttribute("SignatureFormat");
 
         return stringResult;
     }
@@ -59,7 +73,7 @@ public class InvalidDocumentFormatTests extends PdfValidatorSoapTests {
         String detailedReportString = XmlUtil.findElementByXPath(document, "//xmlSimpleReport").getTextContent();
         return XmlUtil.parseXml(detailedReportString);
     }
-	
+
 	private String soapFaultstring(String httpBody) {
 		Document document = XmlUtil.parseXml(httpBody);
         String stringResult = XmlUtil.findElementByXPath(

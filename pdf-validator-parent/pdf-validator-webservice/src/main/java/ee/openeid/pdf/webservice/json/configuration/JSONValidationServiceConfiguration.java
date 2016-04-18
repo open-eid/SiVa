fcfade79926a1b07ac02d98a6046237fa1d4ac2b
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-04-18 16:06:43 +0300

    VAL-152 TSL loading fix for pdf-validator-webservice

--- pdf-validator-parent/pdf-validator-webservice/src/main/java/ee/openeid/pdf/webservice/json/configuration/JSONValidationServiceConfiguration.java
+++ pdf-validator-parent/pdf-validator-webservice/src/main/java/ee/openeid/pdf/webservice/json/configuration/JSONValidationServiceConfiguration.java
@@ -1,18 +1,13 @@
 package ee.openeid.pdf.webservice.json.configuration;
 
 import ee.openeid.pdf.webservice.json.converter.XMLConverter;
-import eu.europa.esig.dss.validation.CertificateVerifier;
-import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
+@ImportResource("classpath*:pdf-validator-webservice.xml")
 public class JSONValidationServiceConfiguration {
-
-    @Bean
-    public CertificateVerifier certificateVerifier() {
-        return new CommonCertificateVerifier();
-    }
 
     @Bean
     public XMLConverter converter() {

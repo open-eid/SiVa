3e31cb26b24ac1ef5ee61e63ace88b24e77339ce
Mihkel Selgal <mihkels@users.noreply.github.com>
2016-05-09 13:06:59 +0300

    Delete UnsupportedtypeException.java

--- siva-parent/siva-validation-proxy/src/main/java/ee/openeid/siva/proxy/document/typeresolver/UnsupportedtypeException.java
+++
@@ -1,7 +0,0 @@
-package ee.openeid.siva.proxy.document.typeresolver;
-
-public class UnsupportedTypeException extends RuntimeException {
-
-    public UnsupportedTypeException(String message) {super(message);}
-
-}

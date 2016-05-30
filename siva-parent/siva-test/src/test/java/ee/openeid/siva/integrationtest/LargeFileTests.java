f9434ded59ac74b14ace0649517e122100489be0
aare <aare.nurm@nortal.com>
2016-05-30 14:22:14 +0300

    VAL-186: adding files and enabling PDF-LargeFiles-3 and PDF-LargeFiles-5

--- siva-parent/siva-test/src/test/java/ee/openeid/siva/integrationtest/LargeFileTests.java
+++ siva-parent/siva-test/src/test/java/ee/openeid/siva/integrationtest/LargeFileTests.java
@@ -18,7 +18,7 @@ public class LargeFileTests extends SiVaRestTests{
 
     private static final String TEST_FILES_DIRECTORY = "pdf/large_pdf_files/";
 
-    /**
+    /***
      * TestCaseID: PDF-LargeFiles-1
      *
      * TestType: Automated
@@ -30,14 +30,14 @@ public class LargeFileTests extends SiVaRestTests{
      * Expected Result: Bigger documents with valid signature should pass
      *
      * File: scout_x4-manual-signed_lt_9mb.pdf
-     */
+     ***/
     @Test
     @Ignore("TODO - a new test file is needed; the current one has issues with QC / SSCD")
     public void nineMegabyteFilesWithLtSignatureAreAccepted () {
         assertSomeSignaturesAreValid(postForReport("scout_x4-manual-signed_lt_9mb.pdf"), 1);
     }
 
-    /**
+    /***
      * TestCaseID: PDF-LargeFiles-2
      *
      * TestType: Automated
@@ -49,13 +49,13 @@ public class LargeFileTests extends SiVaRestTests{
      * Expected Result: Bigger documents with valid signature should pass
      *
      * File: scout_x4-manual-signed_lta_9mb.pdf
-     */
+     ***/
     @Test @Ignore("TODO - a new test file is needed; the current one has issues with QC / SSCD")
     public void nineMegabyteFilesWithLtaSignatureAreAccepted () {
         assertSomeSignaturesAreValid(postForReport("scout_x4-manual-signed_lta_9mb.pdf"), 1);
     }
 
-    /**
+    /***
      * TestCaseID: PDF-LargeFiles-3
      *
      * TestType: Automated
@@ -66,14 +66,14 @@ public class LargeFileTests extends SiVaRestTests{
      *
      * Expected Result: Bigger documents with valid signature should pass
      *
-     * File: digidocservice-signed-lt-1-2mb.pdf
-     */
-    @Test @Ignore("TODO - a new test file is needed; the current one has issues with QC / SSCD")
+     * File: singlesignature_lt_1-2mb.pdf
+     ***/
+    @Test
     public void oneMegabyteFilesWithLtSignatureAreAccepted () {
-        assertSomeSignaturesAreValid(postForReport("digidocservice-signed-lt-1-2mb.pdf"), 1);
+        assertAllSignaturesAreValid(postForReport("singlesignature_lt_1-2mb.pdf"));
     }
 
-    /**
+    /***
      * TestCaseID: PDF-LargeFiles-4
      *
      * TestType: Automated
@@ -85,13 +85,13 @@ public class LargeFileTests extends SiVaRestTests{
      * Expected Result: Bigger documents with valid signature should pass
      *
      * File: digidocservice-signed-lta-1-2mb.pdf
-     */
+     ***/
     @Test @Ignore("TODO - a new test file is needed; the current one has issues with QC / SSCD")
     public void oneMegabyteFilesWithLtaSignatureAreAccepted () {
         assertSomeSignaturesAreValid(postForReport("digidocservice-signed-lta-1-2mb.pdf"), 1);
     }
 
-    /**
+    /***
      * TestCaseID: PDF-LargeFiles-5
      *
      * TestType: Automated
@@ -102,14 +102,14 @@ public class LargeFileTests extends SiVaRestTests{
      *
      * Expected Result: Bigger documents with valid signature should pass
      *
-     * File: egovernment-benchmark-lt-3-8mb.pdf
-     */
-    @Test @Ignore("TODO - a new test file is needed; the current one has issues with QC / SSCD")
+     * File: singlesignature_lt_3-8mb.pdf
+     ***/
+    @Test
     public void fourMegabyteFilesWithLtSignatureAreAccepted () {
-        assertSomeSignaturesAreValid(postForReport("egovernment-benchmark-lt-3-8mb.pdf"), 1);
+        assertSomeSignaturesAreValid(postForReport("singlesignature_lt_3-8mb.pdf"), 1);
     }
 
-    /**
+    /***
      * TestCaseID: PDF-LargeFiles-6
      *
      * TestType: Automated
@@ -121,7 +121,7 @@ public class LargeFileTests extends SiVaRestTests{
      * Expected Result: Bigger documents with valid signature should pass
      *
      * File: egovenrment-benchmark-lta-3-8mb.pdf
-     */
+     ***/
     @Test @Ignore("TODO - a new test file is needed; the current one has issues with QC / SSCD")
     public void fourMegabyteFilesWithLtaSignatureAreAccepted () {
         assertSomeSignaturesAreValid(postForReport("egovenrment-benchmark-lta-3-8mb.pdf"), 1);

cc7c809ef4645580badbef55fa37d6ceb4d3ca06
Andres Voll <Andres.Voll@nortal.com>
2016-06-01 15:31:06 +0300

    VAL-203 Edited BDOC validation service tests

--- validation-services-parent/bdoc-validation-service/src/test/java/ee/openeid/validation/service/bdoc/BDOCValidationServiceTest.java
+++ validation-services-parent/bdoc-validation-service/src/test/java/ee/openeid/validation/service/bdoc/BDOCValidationServiceTest.java
@@ -73,6 +73,7 @@ public class BDOCValidationServiceTest {
         assertEquals("QES", sig1.getSignatureLevel());
         assertEquals("JUHANSON,ALLAN,38608014910", sig1.getSignedBy());
         assertEquals(SignatureValidationData.Indication.TOTAL_PASSED.toString(), sig1.getIndication());
+        assertNull(sig1.getSubIndication());
         assertTrue(sig1.getErrors().size() == 0);
         assertTrue(sig1.getWarnings().size() == 0);
         assertTrue(sig1.getSignatureScopes().size() == 1);
@@ -96,6 +97,7 @@ public class BDOCValidationServiceTest {
         assertEquals("QES", sig2.getSignatureLevel());
         assertEquals("VOLL,ANDRES,39004170346", sig2.getSignedBy());
         assertEquals(SignatureValidationData.Indication.TOTAL_PASSED.toString(), sig2.getIndication());
+        assertNull(sig2.getSubIndication());
         assertTrue(sig2.getErrors().size() == 0);
         assertTrue(sig2.getWarnings().size() == 0);
         assertTrue(sig2.getSignatureScopes().size() == 1);

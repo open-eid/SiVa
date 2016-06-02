190074128d5b54ac05969a278ed9f94f742c3c8c
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-06-02 22:24:36 +0300

    VAL-227 Deploy to demo environment with every git push

--- siva-parent/siva-distribution/src/main/resources/install.sh
+++ siva-parent/siva-distribution/src/main/resources/install.sh
@@ -4,9 +4,6 @@
 sudo systemctl stop siva-sample-app
 sudo systemctl stop siva-webapp
 
-# Build new version of SiVa
-./mvnw clean package -q
-
 # Start up services again
 sudo systemctl start siva-sample-app
 sudo systemctl start siva-webapp

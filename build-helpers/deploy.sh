3c80b9e586e8ad887b403fd788acd0ab7198c316
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-06-08 08:25:33 +0300

    VAL-236 Fixed path to deployment-setup.sh script

--- build-helpers/deploy.sh
+++ build-helpers/deploy.sh
@@ -2,4 +2,4 @@
 
 scp -i ci-deploy-key artifacts/distribution.zip ci-deploy@demoapps.eu:/home/ci-deploy
 scp -i ci-deploy-key ci-build-info.yml ci-deploy@demoapps.eu:/home/ci-deploy
-ssh -i ci-deploy-key ci-deploy@demoapps.eu "bash -s" < ./deploy-setup.sh
+ssh -i ci-deploy-key ci-deploy@demoapps.eu "bash -s" < ./build-helpers/deploy-setup.sh

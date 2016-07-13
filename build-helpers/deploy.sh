3a73a3d90420c2d251f0741e32875e99f9fab87a
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-07-13 11:59:40 +0300

    MAINTENANCE: Staging deployment and system testing

--- build-helpers/deploy.sh
+++ build-helpers/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 scp -i ci-deploy-key artifacts/distribution.zip ci-deploy@demoapps.eu:/home/ci-deploy
-scp -i ci-deploy-key ci-build-info.yml ci-deploy@demoapps.eu:/home/ci-deploy
 ssh -i ci-deploy-key ci-deploy@demoapps.eu "bash -s" < ./build-helpers/deploy-setup.sh
---
+++ build-helpers/stage-deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+ls -al
+
+unzip -o distribution.zip
+
+# Rename applications
+find . -name "siva-webapp*.jar" -exec sh -c 'mv "$1" "siva-webapp.jar"' _ {} \;
+find . -name "siva-sample-application*.jar" -exec sh -c 'mv "$1" "siva-sample-application.jar"' _ {} \;
+
+# Copy webapp to testing directory
+cp siva-webapp.jar /var/apps/stage
+
+# Start test web service
+sudo systemctl start siva-test-webapp
+
+# Wait for test service to start
+sleep 40
+
+# Run system tests
+pyresttest http://demoapps.eu:7000 ${TRAVIS_BUILD_DIR}build-helpers/system-test.yml
+
+# Remove files after testing
+sudo systemctl stop siva-test-webapp
+rm /var/apps/stage/siva-webapp.jar
+
+rm distribution.zip

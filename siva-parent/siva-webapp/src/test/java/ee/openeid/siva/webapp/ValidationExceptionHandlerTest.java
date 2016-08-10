c30c18004c39f96c941e00c7b1436b265bd6956b
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-08-10 09:59:25 +0300

    MAINTENANCE:  Added exception handler unit tests

--- siva-parent/siva-webapp/src/test/java/ee/openeid/siva/webapp/ValidationExceptionHandlerTest.java
+++ siva-parent/siva-webapp/src/test/java/ee/openeid/siva/webapp/ValidationExceptionHandlerTest.java
@@ -4,6 +4,7 @@ import ee.openeid.siva.proxy.ValidationProxy;
 import ee.openeid.siva.proxy.document.ProxyDocument;
 import ee.openeid.siva.validation.exception.MalformedDocumentException;
 import ee.openeid.siva.validation.exception.ValidationServiceException;
+import ee.openeid.siva.validation.service.signature.policy.InvalidPolicyException;
 import ee.openeid.siva.webapp.transformer.ValidationRequestToProxyDocumentTransformer;
 import org.json.JSONObject;
 import org.junit.Before;
@@ -18,6 +19,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -63,6 +65,20 @@ public class ValidationExceptionHandlerTest {
 
         String content = result.getResponse().getContentAsString();
         assertEquals(content, "{\"requestErrors\":[{\"key\":\"document\",\"message\":\"document malformed or not matching documentType\"}]}");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void givenInvalidPolicyWillReturnErrorJson() throws Exception {
+        when(validationProxy.validate(any(ProxyDocument.class))).thenThrow(InvalidPolicyException.class);
+
+        MvcResult mvcResult = mockMvc.perform(post("/validate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request().toString().getBytes()))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("signaturePolicy");
     }
 
     @SuppressWarnings("unchecked")

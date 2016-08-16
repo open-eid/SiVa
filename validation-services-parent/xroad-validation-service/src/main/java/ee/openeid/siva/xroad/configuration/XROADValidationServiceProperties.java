ff500ebcbb56b86307f90422801cd2bf690b8cae
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-08-16 10:38:19 +0300

    MAINTENANCE: Remove default configuration after shutdown

--- validation-services-parent/xroad-validation-service/src/main/java/ee/openeid/siva/xroad/configuration/XROADValidationServiceProperties.java
+++ validation-services-parent/xroad-validation-service/src/main/java/ee/openeid/siva/xroad/configuration/XROADValidationServiceProperties.java
@@ -1,6 +1,10 @@
 package ee.openeid.siva.xroad.configuration;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +12,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.zeroturnaround.zip.ZipUtil;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,12 +27,29 @@ public class XROADValidationServiceProperties {
 
     private static final String DEFAULT_CONFIGURATION_DIRECTORY = "/verificationconf";
     private static final String JAR_PACKAGED_CONFIGURATION = "/xroad-conf.zip";
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private boolean isDefaultConfiguration = false;
     private String configurationDirectoryPath;
 
     @PostConstruct
     public void setDefaultPath() {
         if (configurationDirectoryPath == null) {
             configurationDirectoryPath = unpackPackagedConfiguration();
+            isDefaultConfiguration = true;
+            LOGGER.info("Setting default XROAD configuration path: {}", configurationDirectoryPath);
+        }
+    }
+
+    @PreDestroy
+    public void destroy() throws IOException {
+        if (isDefaultConfiguration) {
+            LOGGER.info("Starting to remove XROAD configuration directory in path: {}", configurationDirectoryPath);
+            int substringEnd = configurationDirectoryPath.lastIndexOf(DEFAULT_CONFIGURATION_DIRECTORY);
+            String tempDirectoryPath = configurationDirectoryPath.substring(0, substringEnd);
+
+            FileUtils.deleteDirectory(new File(tempDirectoryPath));
         }
     }
 
@@ -41,9 +63,9 @@ public class XROADValidationServiceProperties {
             ZipUtil.unpack(new File(zipFilename.toAbsolutePath().toString()), tempXroadConfiguration.toFile());
 
             LOGGER.info("XROAD Configuration directory: {}", zipFilename);
-            return tempXroadConfiguration.toAbsolutePath().toString() + File.separator + DEFAULT_CONFIGURATION_DIRECTORY;
+            return tempXroadConfiguration.toAbsolutePath().toString() + DEFAULT_CONFIGURATION_DIRECTORY;
         } catch (IOException e) {
-            LOGGER.error("XROAD default configuration unpacking failed with error: {}", e.getMessage());
+            LOGGER.error("XROAD default configuration unpacking failed with error: {}", e.getMessage(), e);
         } finally {
             if (tempXroadConfiguration != null) {
                 tempXroadConfiguration.toFile().deleteOnExit();

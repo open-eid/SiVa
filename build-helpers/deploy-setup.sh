77b8519150911dc5679932e1156811e5ce9b2511
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-08-16 23:33:52 +0300

    MAINTENANCE: Build and publish docker images and deploy to demoapps.eu

--- build-helpers/deploy-setup.sh
+++ build-helpers/deploy-setup.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 # Copy JAR files to apps directory
-mv ci-build-info.yml /var/apps
-mv *.jar /var/apps
-ls -al /var/apps
-
-rm /var/apps/*.log
+#mv ci-build-info.yml /var/apps
+#mv *.jar /var/apps
+#ls -al /var/apps
+#
+#rm /var/apps/*.log
 
 # Remove keystore directory
-rm -fr /var/apps/etc
+#rm -fr /var/apps/etc
 
 # Run installation of new apps
-chmod +x ./install.sh && ./install.sh
+#chmod +x ./install.sh && ./install.sh
 
+docker-compose stop && docker-compose rm -f -a
+docker-compose start

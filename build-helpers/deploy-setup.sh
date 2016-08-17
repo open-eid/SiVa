b73d7381ca130a2f8829ea136e9b2e047d85c46d
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-08-17 10:00:10 +0300

    MAINTENANCE: Updated deployment process

--- build-helpers/deploy-setup.sh
+++ build-helpers/deploy-setup.sh
@@ -13,5 +13,14 @@
 # Run installation of new apps
 #chmod +x ./install.sh && ./install.sh
 
-docker-compose stop && docker-compose rm -f -a
-docker-compose start
+# Add docker compose to $PATH
+export PATH=$PATH:/usr/local/bin
+
+# Stop all SiVa validation services and remove them
+docker-compose stop && docker-compose rm -f
+
+# Pull latest docker image
+docker-compose pull
+
+# Start all SiVa validation services
+docker-compose up -d

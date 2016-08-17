5146074e6fdcd94fd154f0a46cfad17186d0d912
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-08-17 12:22:41 +0300

    MAINTENANCE: Deployment code cleanup and responsive view fixes for sample application

--- build-helpers/deploy-setup.sh
+++ build-helpers/deploy-setup.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-# Copy JAR files to apps directory
-#mv ci-build-info.yml /var/apps
-#mv *.jar /var/apps
-#ls -al /var/apps
-#
-#rm /var/apps/*.log
-
-# Remove keystore directory
-#rm -fr /var/apps/etc
-
-# Run installation of new apps
-#chmod +x ./install.sh && ./install.sh
+ls -al
 
 # Add docker compose to $PATH
 export PATH=$PATH:/usr/local/bin
--- build-helpers/system-test-prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir ${TRAVIS_BUILD_DIR}/artifacts && cp ${TRAVIS_BUILD_DIR}/siva-parent/siva-distribution/target/*.zip artifacts
-mv ${TRAVIS_BUILD_DIR}/artifacts/*.zip ${TRAVIS_BUILD_DIR}/artifacts/distribution.zip && ls -al ${TRAVIS_BUILD_DIR}/artifacts
-chmod 600 ${TRAVIS_BUILD_DIR}/ci-deploy-key
-
-scp -i ci-deploy-key artifacts/distribution.zip ci-deploy@demoapps.eu:/home/ci-deploy
-ssh -i ci-deploy-key ci-deploy@demoapps.eu "bash -s" < ./build-helpers/stage-deploy.sh

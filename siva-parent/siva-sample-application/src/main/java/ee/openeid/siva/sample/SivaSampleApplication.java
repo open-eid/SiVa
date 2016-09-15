7ec93c4ad702b6f630d09fc4ba90894ec6b18301
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-09-15 22:32:46 +0300

    MAINTENANCE: Added support to generate WAR files when running maven build with war profile

--- siva-parent/siva-sample-application/src/main/java/ee/openeid/siva/sample/SivaSampleApplication.java
+++ siva-parent/siva-sample-application/src/main/java/ee/openeid/siva/sample/SivaSampleApplication.java
@@ -2,11 +2,18 @@ package ee.openeid.siva.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
 @SpringBootApplication
-public class SivaSampleApplication {
+public class SivaSampleApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SivaSampleApplication.class);
+    }
 
     public static void main(String... args) {
         SpringApplication.run(SivaSampleApplication.class, args);

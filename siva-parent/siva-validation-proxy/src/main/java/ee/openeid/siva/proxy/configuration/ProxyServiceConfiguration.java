ee68dee431e4840de6acadf3e1a5eef77b72a642
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-09-20 09:47:31 +0300

    VAL-333 Log out XRoad service URL

--- siva-parent/siva-validation-proxy/src/main/java/ee/openeid/siva/proxy/configuration/ProxyServiceConfiguration.java
+++ siva-parent/siva-validation-proxy/src/main/java/ee/openeid/siva/proxy/configuration/ProxyServiceConfiguration.java
@@ -1,6 +1,8 @@
 package ee.openeid.siva.proxy.configuration;
 
 import ee.openeid.siva.proxy.http.RestProxyErrorHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -12,11 +14,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @EnableConfigurationProperties({ProxyConfigurationProperties.class})
 public class ProxyServiceConfiguration {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProxyServiceConfiguration.class);
     private ProxyConfigurationProperties properties;
 
     @Bean
     public RestTemplate restTemplate() {
+        LOGGER.info("XRoad Validation service base URL: {}", properties.getXroadUrl());
         return new RestTemplateBuilder()
                 .rootUri(properties.getXroadUrl())
                 .errorHandler(responseErrorHandler())

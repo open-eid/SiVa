4193c50c35a1c3d2d7e3a2750593156976619c6b
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-09-27 16:37:27 +0300

    VAL-340 Added more missing license headers in source files

--- siva-parent/siva-test/src/test/java/ee/openeid/siva/resttest/ValidationRequestIT.java
+++ siva-parent/siva-test/src/test/java/ee/openeid/siva/resttest/ValidationRequestIT.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Riigi Infosüsteemide Amet
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+
 package ee.openeid.siva.resttest;
 
 import com.jayway.restassured.RestAssured;
@@ -8,7 +24,6 @@ import org.hamcrest.Matchers;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.http.HttpStatus;
@@ -19,7 +34,6 @@ import java.util.Map;
 import static com.jayway.restassured.path.json.JsonPath.from;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertTrue;
-
 
 @Category(IntegrationTest.class)
 public class ValidationRequestIT extends SiVaRestTests {
--- validation-services-parent/validation-commons/src/main/java/ee/openeid/siva/validation/util/CertUtil.java
+++ validation-services-parent/validation-commons/src/main/java/ee/openeid/siva/validation/util/CertUtil.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2016 Riigi Infosüsteemide Amet
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+
 package ee.openeid.siva.validation.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.bouncycastle.asn1.x500.RDN;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.style.BCStyle;
@@ -11,7 +29,8 @@ import org.slf4j.LoggerFactory;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 
-public class CertUtil {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CertUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CertUtil.class);
 

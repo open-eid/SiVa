a804c712706f5267678191028f51aa2c3a6e2766
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-09-27 15:15:43 +0300

    VAL-332 Changed Spring Boot version to 1.4.1 and possible fix to front-end lib loading

--- siva-parent/siva-sample-application/src/main/resources/static/js/google-tracking-init.js
+++ siva-parent/siva-sample-application/src/main/resources/static/js/google-tracking-init.js
@@ -1,3 +1,20 @@
+
+/*
+ * Copyright 2016 Riigi Infosüsteemide Amet
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+
 //noinspection JSLint,JSLint,JSLint,JSLint,JSLint,JSLint,JSLint,JSLint
 (function(i,s,o,g,r,a,m){//noinspection JSLint,JSLint,JSLint,JSLint
     i['GoogleAnalyticsObject']=r;//noinspection CommaExpressionJS // jshint ignore:line // jshint ignore:line,JSHint,JSHint,JSLint,JSLint,JSLint,JSLint,JSLint,JSLint,JSLint,JSLint,JSLint,JSLint

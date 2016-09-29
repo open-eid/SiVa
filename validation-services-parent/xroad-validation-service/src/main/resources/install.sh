83478574f27991a12f6d24ba8525e6bdfa2cac9d
Mihkel Selgal <mihkel.selgal@nortal.com>
2016-09-29 16:42:01 +0300

    VAL-345 Changed groupID to ee.openeid.siva

--- validation-services-parent/xroad-validation-service/src/main/resources/install.sh
+++ validation-services-parent/xroad-validation-service/src/main/resources/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn install:install-file -DlocalRepositoryPath=../../../external-lib-repository -DcreateChecksum=true -Dpackaging=jar -Dfile=./xroad-jars/asic-verifier-1.0-mod.jar -DgroupId=io.github.openeid.siva -DartifactId=xroad-asice-verifier -Dversion=1.0
+mvn install:install-file -DlocalRepositoryPath=../../../external-lib-repository -DcreateChecksum=true -Dpackaging=jar -Dfile=./xroad-jars/asic-verifier-1.0-mod.jar -DgroupId=ee.openid.siva -DartifactId=xroad-asice-verifier -Dversion=1.0

54232833921a0243ad23d2efed6ab96dc1f946cd
priit.reiser <priitr@laptop833.webmedia.int>
2016-10-24 17:05:30 +0300

    removed demoapps.eu deployment in travis build

--- build-helpers/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-scp -i ci-deploy-key docker-compose.yml ci-deploy@demoapps.eu:/home/ci-deploy
-ssh -i ci-deploy-key ci-deploy@demoapps.eu "bash -s" < ./build-helpers/deploy-setup.sh

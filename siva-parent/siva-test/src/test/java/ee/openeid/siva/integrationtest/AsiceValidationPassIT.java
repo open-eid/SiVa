2ae287b19689f7442d929a536d110f2fd165acc9
aare <aare.nurm@nortal.com>
2017-10-26 14:30:26 +0300

    SIVARIA2-34 addition of asice validation pass tests

---
+++ siva-parent/siva-test/src/test/java/ee/openeid/siva/integrationtest/AsiceValidationPassIT.java
@@ -0,0 +1,320 @@
+/*
+ * Copyright 2016 Riigi Infosüsteemide Amet
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+
+package ee.openeid.siva.integrationtest;
+
+import com.jayway.restassured.RestAssured;
+import ee.openeid.siva.integrationtest.configuration.IntegrationTest;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(IntegrationTest.class)
+public class AsiceValidationPassIT extends SiVaRestTests {
+    private static final String DEFAULT_TEST_FILES_DIRECTORY = "bdoc/live/timestamp/";
+    private String testFilesDirectory = DEFAULT_TEST_FILES_DIRECTORY;
+
+    @BeforeClass
+    public static void oneTimeSetUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Before
+    public void DirectoryBackToDefault() {
+        setTestFilesDirectory(DEFAULT_TEST_FILES_DIRECTORY);
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-1
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice with single valid signature
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: ValidLiveSignature.asice
+     */
+    @Test
+    public void validAsiceSingleSignature() {
+        assertAllSignaturesAreValid(postForReport("ValidLiveSignature.asice"));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-2
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice TM with multiple valid signatures
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: BDOC-TS.asice
+     */
+    @Test
+    public void validAsiceMultipleSignatures() {
+        assertAllSignaturesAreValid(postForReport("BDOC-TS.asice"));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-3
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice with warning on signature
+     *
+     * Expected Result: The document should pass the validation but warning should be returned
+     *
+     * File: bdoc_weak_warning_sha1.bdoc
+     */
+    @Test
+    @Ignore //TODO: New file needed. This one has different mimetype value in manifest.xml and signature.xml
+    public void validSignatureWithWarning() {
+        setTestFilesDirectory("bdoc/live/timemark/");
+        post(validationRequestFor("bdoc_weak_warning_sha1.bdoc"))
+                .then()
+                .body("signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("signatures[0].subIndication", Matchers.is(""))
+                .body("validSignaturesCount", Matchers.is(1));
+
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-4
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice One LT signature with certificates from different countries
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: EE_SER-AEX-B-LT-V-30.asice
+     */
+    @Test
+    public void asiceDifferentCertificateCountries() {
+        post(validationRequestFor("EE_SER-AEX-B-LT-V-30.asice"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1));
+    }
+
+
+    /**
+     * TestCaseID: Asice-ValidationPass-5
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice Baseline-LT file
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: EE_SER-AEX-B-LT-V-49.asice
+     */
+    @Test
+    public void asiceBaselineLtProfileValidSignature() {
+        post(validationRequestFor("EE_SER-AEX-B-LT-V-49.asice"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_LT"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-6
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice Baseline-LTA file
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: EE_SER-AEX-B-LTA-V-24.asice
+     */
+    @Test
+    public void asiceBaselineLtaProfileValidSignature() {
+        post(validationRequestFor("EE_SER-AEX-B-LTA-V-24.asice"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_LTA"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-7
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice file signed with Mobile-ID, ECC-SHA256 signature with prime256v1 key
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: EE_SER-AEX-B-LT-V-2.asice
+     */
+    @Test
+    public void asiceWithEccSha256ValidSignature() {
+        post(validationRequestFor("EE_SER-AEX-B-LT-V-2.asice"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_LT"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-8
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice file with 	ESTEID-SK 2015 certificate chain
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: IB-4270_TS_ESTEID-SK 2015  SK OCSP RESPONDER 2011.asice
+     */
+    @Test
+    public void asiceSk2015CertificateChainValidSignature() {
+        post(validationRequestFor("IB-4270_TS_ESTEID-SK 2015  SK OCSP RESPONDER 2011.asice"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_LT"))
+                .body("validationReport.validationConclusion.signatures[0].signatureLevel", Matchers.is("QESIG"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-9
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#siva-signature-validation-policy-version-2-polv4
+     *
+     * Title: Asice file with KLASS3-SK 2010 (EECCRCA) certificate chain
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: EE_SER-AEX-B-LT-V-28.asice
+     */
+    @Test
+    public void asiceKlass3Sk2010CertificateChainValidSignature() {
+        post(validationRequestFor("EE_SER-AEX-B-LT-V-28.asice"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_LT"))
+                .body("validationReport.validationConclusion.signatures[0].signatureLevel", Matchers.is("QES"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-10
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#common-validation-constraints-polv3-polv4
+     *
+     * Title: *.sce file with TimeStamp
+     *
+     * Expected Result: The document should pass the validation
+     *
+     * File: ASICE_TS_LTA_content_as_sce.sce
+     */
+    @Test
+    public void asiceWithSceFileExtensionShouldPass() {
+        post(validationRequestFor("ASICE_TS_LTA_content_as_sce.sce"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_LTA"))
+                .body("validationReport.validationConclusion.signatures[0].signatureLevel", Matchers.is("QESIG"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1))
+                .body("validationReport.validationConclusion.signaturesCount", Matchers.is(1));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-11
+     *
+     * TestType: Automated
+     *
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#common-validation-constraints-polv3-polv4
+     *
+     * Title: Asice-TS with special characters in data file
+     *
+     * Expected Result: The document should pass the validation with correct signature scope
+     *
+     * File: Nonconventionalcharacters.asice
+     */
+    @Test
+    public void asiceWithSpecialCharactersInDataFileShouldPass() {
+        post(validationRequestFor("Nonconventionalcharacters.asice"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_LT"))
+                .body("validationReport.validationConclusion.signatures[0].signatureLevel", Matchers.is("QESIG"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].name", Matchers.is("!~#¤%%&()=+-_.txt"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].scope", Matchers.is("FullSignatureScope"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].content", Matchers.is("Full document"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1))
+                .body("validationReport.validationConclusion.signaturesCount", Matchers.is(1));
+    }
+
+    /**
+     * TestCaseID: Asice-ValidationPass-12
+     * <p>
+     * TestType: Automated
+     * <p>
+     * Requirement: http://open-eid.github.io/SiVa/siva/appendix/validation_policy/#common-validation-constraints-polv3-polv4
+     * <p>
+     * Title: Asice unsigned data files in the container
+     * <p>
+     * Expected Result: The document should pass the validation with warning
+     * <p>
+     * File: EE_SER-AEX-B-LT-V-34.asice
+     */
+    @Test
+    public void asiceUnsignedDataFiles() {
+        post(validationRequestFor("EE_SER-AEX-B-LT-V-34.asice"))
+                .then()
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.signatures[0].warnings[0].content", Matchers.is("All files are not signed!"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1));
+
+    }
+
+    @Override
+    protected String getTestFilesDirectory() {
+        return testFilesDirectory;
+    }
+
+    public void setTestFilesDirectory(String testFilesDirectory) {
+        this.testFilesDirectory = testFilesDirectory;
+    }
+}

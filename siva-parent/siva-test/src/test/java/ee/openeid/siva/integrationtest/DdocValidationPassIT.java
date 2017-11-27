11163e43f69c725c2e1afbdcfca3718c948a8a33
aare <aare.nurm@nortal.com>
2017-11-27 13:58:47 +0200

    MAINTENANCE DDOC hashcode tests enabled

--- siva-parent/siva-test/src/test/java/ee/openeid/siva/integrationtest/DdocValidationPassIT.java
+++ siva-parent/siva-test/src/test/java/ee/openeid/siva/integrationtest/DdocValidationPassIT.java
@@ -311,27 +311,28 @@ public class DdocValidationPassIT extends SiVaRestTests{
      * File: SK-XML1.0_hashcode.ddoc
      *
      */
-    @Test @Ignore //TODO: https://github.com/open-eid/SiVa/issues/11
+    @Ignore //TODO: https://github.com/open-eid/SiVa/issues/11
+    @Test
     public void ddocV1_0HashcodeShouldPass() {
         post(validationRequestFor("SK-XML1_0_hashcode.ddoc"))
                 .then()
-                .body("signatures[0].id", Matchers.is("S0"))
-                .body("signatures[0].signatureFormat", Matchers.is("SK_XML_1.0"))
-                .body("signatures[0].signatureLevel", Matchers.is(""))
-                .body("signatures[0].signedBy", Matchers.is("ANSIP,ANDRUS,35610012722"))
-                .body("signatures[0].indication", Matchers.is("TOTAL-PASSED"))
-                .body("signatures[0].subIndication", Matchers.is(""))
-                .body("signatures[0].errors", Matchers.hasSize(0))
-                .body("signatures[0].signatureScopes[0].name", Matchers.is(""))
-                .body("signatures[0].signatureScopes[0].scope", Matchers.is(""))
-                .body("signatures[0].signatureScopes[0].content", Matchers.is(""))
-                .body("signatures[0].claimedSigningTime", Matchers.is("2002-10-07T12:10:19Z"))
-                .body("signatures[0].warnings", Matchers.hasSize(0))
-                .body("signatures[0].info.bestSignatureTime", Matchers.is(""))
-                .body("signatureForm", Matchers.is("DIGIDOC_XML_1.0_hashcode"))
-                .body("filename", Matchers.is("SK-XML1.0_hashcode.ddoc"))
-                .body("validSignaturesCount", Matchers.is(2))
-                .body("signaturesCount", Matchers.is(2));
+                .body("validationReport.validationConclusion.signatures[0].id", Matchers.is("S0"))
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("SK_XML_1.0"))
+                .body("validationReport.validationConclusion.signatures[0].signatureLevel", Matchers.is(""))
+                .body("validationReport.validationConclusion.signatures[0].signedBy", Matchers.is("ANSIP,ANDRUS,35610012722"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.signatures[0].subIndication", Matchers.is(""))
+                .body("validationReport.validationConclusion.signatures[0].errors", Matchers.hasSize(0))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].name", Matchers.is(""))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].scope", Matchers.is(""))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].content", Matchers.is(""))
+                .body("validationReport.validationConclusion.signatures[0].claimedSigningTime", Matchers.is("2002-10-07T12:10:19Z"))
+                .body("validationReport.validationConclusion.signatures[0].warnings", Matchers.hasSize(0))
+                .body("validationReport.validationConclusion.signatures[0].info.bestSignatureTime", Matchers.is(""))
+                .body("validationReport.validationConclusion.signatureForm", Matchers.is("DIGIDOC_XML_1.0_hashcode"))
+                .body("validationReport.validationConclusion.validatedDocument.filename", Matchers.is("SK-XML1.0_hashcode.ddoc"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(2))
+                .body("validationReport.validationConclusion.signaturesCount", Matchers.is(2));
     }
 
     /**
@@ -349,27 +350,24 @@ public class DdocValidationPassIT extends SiVaRestTests{
      * File: DIGIDOC-XML1.1_hashcode.ddoc
      *
      */
-    @Test @Ignore //TODO: This tests fails in Travis. Has been executed locally
+    @Test
     public void ddocV1_1HashcodeShouldPass() {
         post(validationRequestFor("DIGIDOC-XML1.1_hashcode.ddoc"))
                 .then()
-                .body("signatures[0].id", Matchers.is("S0"))
-                .body("signatures[0].signatureFormat", Matchers.is("DIGIDOC_XML_1.1"))
-                .body("signatures[0].signatureLevel", Matchers.is(""))
-                .body("signatures[0].signedBy", Matchers.is("KESKEL,URMO,38002240232"))
-                .body("signatures[0].indication", Matchers.is("TOTAL-PASSED"))
-                .body("signatures[0].subIndication", Matchers.is(""))
-                .body("signatures[0].errors", Matchers.hasSize(0))
-                .body("signatures[0].signatureScopes[0].name", Matchers.is("puhkus_urmo_062006.doc"))
-                .body("signatures[0].signatureScopes[0].scope", Matchers.is("FullSignatureScope"))
-                .body("signatures[0].signatureScopes[0].content", Matchers.is("Full document"))
-                .body("signatures[0].claimedSigningTime", Matchers.is("2006-06-26T12:15:40Z"))
-                .body("signatures[0].warnings", Matchers.hasSize(0))
-                .body("signatures[0].info.bestSignatureTime", Matchers.is(""))
-                .body("signatureForm", Matchers.is("DIGIDOC_XML_1.1_hashcode"))
-                .body("filename", Matchers.is("DIGIDOC-XML1.1_hashcode.ddoc"))
-                .body("validSignaturesCount", Matchers.is(1))
-                .body("signaturesCount", Matchers.is(1));
+                .body("validationReport.validationConclusion.signatures[0].id", Matchers.is("S0"))
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("DIGIDOC_XML_1.1"))
+                .body("validationReport.validationConclusion.signatures[0].signedBy", Matchers.is("KESKEL,URMO,38002240232"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.signatures[0].errors", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].name", Matchers.is("puhkus_urmo_062006.doc"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].scope", Matchers.is("FullSignatureScope"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].content", Matchers.is("Full document"))
+                .body("validationReport.validationConclusion.signatures[0].claimedSigningTime", Matchers.is("2006-06-26T12:15:40Z"))
+                .body("validationReport.validationConclusion.signatures[0].warnings", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatureForm", Matchers.is("DIGIDOC_XML_1.1_hashcode"))
+                .body("validationReport.validationConclusion.validatedDocument.filename", Matchers.is("DIGIDOC-XML1.1_hashcode.ddoc"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1))
+                .body("validationReport.validationConclusion.signaturesCount", Matchers.is(1));
     }
 
     /**
@@ -387,27 +385,26 @@ public class DdocValidationPassIT extends SiVaRestTests{
      * File: DIGIDOC-XML1.2_hashcode.ddoc
      *
      */
-    @Test @Ignore //TODO: This tests fails in Travis. Has been executed locally
+    @Test
     public void ddocV1_2HashcodeShouldPass() {
         post(validationRequestFor("DIGIDOC-XML1.2_hashcode.ddoc"))
                 .then()
-                .body("signatures[0].id", Matchers.is("S0"))
-                .body("signatures[0].signatureFormat", Matchers.is("DIGIDOC_XML_1.2"))
-                .body("signatures[0].signatureLevel", Matchers.is(""))
-                .body("signatures[0].signedBy", Matchers.is("Eesti Ühispank: Ülekandejuhise kinnitus"))
-                .body("signatures[0].indication", Matchers.is("TOTAL-PASSED"))
-                .body("signatures[0].subIndication", Matchers.is(""))
-                .body("signatures[0].errors", Matchers.hasSize(0))
-                .body("signatures[0].signatureScopes[0].name", Matchers.is("RO219559508.pdf"))
-                .body("signatures[0].signatureScopes[0].scope", Matchers.is("FullSignatureScope"))
-                .body("signatures[0].signatureScopes[0].content", Matchers.is("Full document"))
-                .body("signatures[0].claimedSigningTime", Matchers.is("2003-10-24T10:57:19Z"))
-                .body("signatures[0].warnings", Matchers.hasSize(0))
-                .body("signatures[0].info.bestSignatureTime", Matchers.is(""))
-                .body("signatureForm", Matchers.is("DIGIDOC_XML_1.2_hashcode"))
-                .body("filename", Matchers.is("DIGIDOC-XML1.2_hashcode.ddoc"))
-                .body("validSignaturesCount", Matchers.is(1))
-                .body("signaturesCount", Matchers.is(1));
+                .body("validationReport.validationConclusion.signatures[0].id", Matchers.is("S0"))
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("DIGIDOC_XML_1.2"))
+                .body("validationReport.validationConclusion.signatures[0].signatureLevel", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatures[0].signedBy", Matchers.is("Eesti Ühispank: Ülekandejuhise kinnitus"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.signatures[0].subIndication", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatures[0].errors", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].name", Matchers.is("RO219559508.pdf"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].scope", Matchers.is("FullSignatureScope"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].content", Matchers.is("Full document"))
+                .body("validationReport.validationConclusion.signatures[0].claimedSigningTime", Matchers.is("2003-10-24T10:57:19Z"))
+                .body("validationReport.validationConclusion.signatures[0].warnings", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatureForm", Matchers.is("DIGIDOC_XML_1.2_hashcode"))
+                .body("validationReport.validationConclusion.validatedDocument.filename", Matchers.is("DIGIDOC-XML1.2_hashcode.ddoc"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1))
+                .body("validationReport.validationConclusion.signaturesCount", Matchers.is(1));
     }
 
     /**
@@ -425,27 +422,26 @@ public class DdocValidationPassIT extends SiVaRestTests{
      * File: DIGIDOC-XML1.3_hashcode.ddoc
      *
      */
-    @Test @Ignore //TODO: This tests fails in Travis. Has been executed locally
+    @Test
     public void ddocV1_3HashcodeShouldPass() {
         post(validationRequestFor("DIGIDOC-XML1.3_hashcode.ddoc"))
                 .then()
-                .body("signatures[0].id", Matchers.is("S0"))
-                .body("signatures[0].signatureFormat", Matchers.is("DIGIDOC_XML_1.3"))
-                .body("signatures[0].signatureLevel", Matchers.is(""))
-                .body("signatures[0].signedBy", Matchers.is("LUKIN,LIISA,47710110274"))
-                .body("signatures[0].indication", Matchers.is("TOTAL-PASSED"))
-                .body("signatures[0].subIndication", Matchers.is(""))
-                .body("signatures[0].errors", Matchers.hasSize(0))
-                .body("signatures[0].signatureScopes[0].name", Matchers.is("Glitter-rock-4_gallery.jpg"))
-                .body("signatures[0].signatureScopes[0].scope", Matchers.is("FullSignatureScope"))
-                .body("signatures[0].signatureScopes[0].content", Matchers.is("Full document"))
-                .body("signatures[0].claimedSigningTime", Matchers.is("2012-10-03T07:46:31Z"))
-                .body("signatures[0].warnings", Matchers.hasSize(0))
-                .body("signatures[0].info.bestSignatureTime", Matchers.is(""))
-                .body("signatureForm", Matchers.is("DIGIDOC_XML_1.3_hashcode"))
-                .body("filename", Matchers.is("DIGIDOC-XML1.3_hashcode.ddoc"))
-                .body("validSignaturesCount", Matchers.is(1))
-                .body("signaturesCount", Matchers.is(1));
+                .body("validationReport.validationConclusion.signatures[0].id", Matchers.is("S0"))
+                .body("validationReport.validationConclusion.signatures[0].signatureFormat", Matchers.is("DIGIDOC_XML_1.3"))
+                .body("validationReport.validationConclusion.signatures[0].signatureLevel", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatures[0].signedBy", Matchers.is("LUKIN,LIISA,47710110274"))
+                .body("validationReport.validationConclusion.signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("validationReport.validationConclusion.signatures[0].subIndication", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatures[0].errors", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].name", Matchers.is("Glitter-rock-4_gallery.jpg"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].scope", Matchers.is("FullSignatureScope"))
+                .body("validationReport.validationConclusion.signatures[0].signatureScopes[0].content", Matchers.is("Full document"))
+                .body("validationReport.validationConclusion.signatures[0].claimedSigningTime", Matchers.is("2012-10-03T07:46:31Z"))
+                .body("validationReport.validationConclusion.signatures[0].warnings", Matchers.isEmptyOrNullString())
+                .body("validationReport.validationConclusion.signatureForm", Matchers.is("DIGIDOC_XML_1.3_hashcode"))
+                .body("validationReport.validationConclusion.validatedDocument.filename", Matchers.is("DIGIDOC-XML1.3_hashcode.ddoc"))
+                .body("validationReport.validationConclusion.validSignaturesCount", Matchers.is(1))
+                .body("validationReport.validationConclusion.signaturesCount", Matchers.is(1));
     }
 
     @Override

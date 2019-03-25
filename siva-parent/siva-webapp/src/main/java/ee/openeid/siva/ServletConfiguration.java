e5988b3499de03747c2c55d002f2580252104655
Jorgen Heinsoo <Jorgen.Heinsoo@nortal.com>
2019-03-25 10:30:30 +0200

    SIVA-109 - Added separate WSDL service description for x-road with x-road headers. Fixed some double generated endpoints.

--- siva-parent/siva-webapp/src/main/java/ee/openeid/siva/ServletConfiguration.java
+++ siva-parent/siva-webapp/src/main/java/ee/openeid/siva/ServletConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Riigi Infosüsteemide Amet
+ * Copyright 2019 Riigi Infosüsteemide Amet
  *
  * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
  * the European Commission - subsequent versions of the EUPL (the "Licence");
@@ -32,12 +32,14 @@ import ee.openeid.siva.webapp.soap.interceptor.ReportSignatureInterceptor;
 import ee.openeid.siva.webapp.soap.interceptor.SoapRequestHashcodeValidationInterceptor;
 import ee.openeid.siva.webapp.soap.interceptor.SoapRequestValidationInterceptor;
 import ee.openeid.siva.webapp.soap.interceptor.SoapResponseHeaderInterceptor;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.cxf.Bus;
 import org.apache.cxf.binding.soap.interceptor.AbstractSoapInterceptor;
 import org.apache.cxf.binding.soap.saaj.SAAJOutInterceptor;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.transport.servlet.CXFServlet;
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringBootConfiguration;
@@ -48,20 +50,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.xml.namespace.QName;
 import javax.xml.ws.Endpoint;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootConfiguration
 @EnableConfigurationProperties({ReportConfigurationProperties.class, ProxyConfigurationProperties.class, WsdlServiceConfigurationProperties.class})
 public class ServletConfiguration extends MonitoringConfiguration {
+
     private static final String VALIDATION_WEB_SERVICE_ENDPOINT = "/validationWebService";
     private static final String HASHCODE_VALIDATION_WEB_SERVICE_ENDPOINT = "/hashcodeValidationWebService";
     private static final String DATAFILES_ENDPOINT = "/dataFilesWebService";
     private static final String URL_MAPPING = "/soap/*";
+    private static final String SIVA_SERVICE_NAMESPACE = "http://soap.webapp.siva.openeid.ee/";
 
     private ProxyConfigurationProperties proxyProperties;
-    private WsdlServiceConfigurationProperties wsdlServiceConfigurationProperties;
+    private WsdlServiceConfigurationProperties wsdlConfProperties;
 
     @Autowired
     @Qualifier("SoapReportSignatureInterceptor")
@@ -95,6 +101,11 @@ public class ServletConfiguration extends MonitoringConfiguration {
     }
 
     @Bean
+    public DataFilesWebService dataFilesWebService() {
+        return new DataFilesWebServiceImpl();
+    }
+
+    @Bean
     public Jackson2ObjectMapperBuilder objectMapperBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
         builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
@@ -103,42 +114,87 @@ public class ServletConfiguration extends MonitoringConfiguration {
 
     @Bean
     public Endpoint validationEndpoint(SpringBus springBus, ValidationWebService validationWebService) {
-        EndpointImpl endpoint = new EndpointImpl(springBus, validationWebService);
-        endpoint.setWsdlLocation("wsdl/siva.wsdl");
-        endpoint.getOutInterceptors().addAll(commonEndpointOutInterceptors());
-        endpoint.getInInterceptors().add(new SoapRequestValidationInterceptor());
-        if (wsdlServiceConfigurationProperties.getValidationWebServiceUrl() != null)
-            endpoint.setPublishedEndpointUrl(wsdlServiceConfigurationProperties.getValidationWebServiceUrl());
-        endpoint.publish(VALIDATION_WEB_SERVICE_ENDPOINT);
-        return endpoint;
+        return constructValidationEndpoint(springBus, validationWebService, "ValidationWebServiceService", null);
+    }
+
+    @Bean
+    public Endpoint validationXRoadEndpoint(SpringBus springBus, ValidationWebService validationWebService) {
+        return constructValidationEndpoint(springBus, validationWebService, "XRoadValidationWebService", "XRoad");
     }
 
     @Bean
     public Endpoint hashcodeValidationEndpoint(SpringBus springBus, HashcodeValidationWebService validationWebService) {
-        EndpointImpl endpoint = new EndpointImpl(springBus, validationWebService);
-        endpoint.setWsdlLocation("wsdl/siva-hashcode-validation.wsdl");
-        if (wsdlServiceConfigurationProperties.getHashcodeValidationWebService() != null)
-            endpoint.setPublishedEndpointUrl(wsdlServiceConfigurationProperties.getHashcodeValidationWebService());
-        endpoint.getOutInterceptors().addAll(commonEndpointOutInterceptors());
-        endpoint.getInInterceptors().add(new SoapRequestHashcodeValidationInterceptor());
-
-        endpoint.publish(HASHCODE_VALIDATION_WEB_SERVICE_ENDPOINT);
-        return endpoint;
+        return constructHashcodeValidationEndpoint(springBus, validationWebService, "HashcodeValidationWebServiceService", null);
     }
 
     @Bean
-    public DataFilesWebService dataFilesWebService() {
-        return new DataFilesWebServiceImpl();
+    public Endpoint hashcodeValidationXRoadEndpoint(SpringBus springBus, HashcodeValidationWebService validationWebService) {
+        return constructHashcodeValidationEndpoint(springBus, validationWebService, "XRoadHashcodeValidationWebService", "XRoad");
     }
 
     @Bean
     public Endpoint dataFilesEndpoint(SpringBus springBus, DataFilesWebService dataFilesWebService) {
-        EndpointImpl endpoint = new EndpointImpl(springBus, dataFilesWebService);
-        endpoint.setWsdlLocation("wsdl/siva-datafiles.wsdl");
-        if (wsdlServiceConfigurationProperties.getDataFilesWebService() != null)
-            endpoint.setPublishedEndpointUrl(wsdlServiceConfigurationProperties.getDataFilesWebService());
-        endpoint.publish(DATAFILES_ENDPOINT);
+        return constructDataFilesEndpoint(springBus, dataFilesWebService, "DataFilesWebServiceService", null);
+    }
+
+    @Bean
+    public Endpoint dataFilesXRoadEndpoint(SpringBus springBus, DataFilesWebService dataFilesWebService) {
+        return constructDataFilesEndpoint(springBus, dataFilesWebService, "XRoadDataFilesWebService", "XRoad");
+    }
+
+    private EndpointImpl constructValidationEndpoint(SpringBus springBus, ValidationWebService validationWebService, String serviceName, String endpointPathExtra) {
+        EndpointImpl endpoint = new EndpointImpl(springBus, validationWebService);
+        endpoint.setWsdlLocation("wsdl/siva.wsdl");
+        endpoint.setServiceName(new QName(SIVA_SERVICE_NAMESPACE, serviceName));
+        endpoint.getOutInterceptors().addAll(commonEndpointOutInterceptors());
+        endpoint.getInInterceptors().add(new SoapRequestValidationInterceptor());
+        if (StringUtils.isNotBlank(wsdlConfProperties.getValidationWebServiceUrl())) {
+            endpoint.setPublishedEndpointUrl(constructPublishedEndpointUrl(wsdlConfProperties.getValidationWebServiceUrl(), VALIDATION_WEB_SERVICE_ENDPOINT));
+        }
+        /**
+         *  Solution for having multiple services defined in the same WSDL go against the same endpoint.
+         *  For that multiple EndpointImpl instances must be constructed pointing to the specific service.
+         *  To construct multiple EndpointImpl instances from the same WSDL the published endpoint name must be different and therefore extra is added.
+         *  It does result with additional unnecessary endpoint for reaching WSDL and XSD.
+         */
+        endpoint.publish(constructPublishingPath(VALIDATION_WEB_SERVICE_ENDPOINT, endpointPathExtra));
         return endpoint;
+    }
+
+    private EndpointImpl constructHashcodeValidationEndpoint(SpringBus springBus, HashcodeValidationWebService validationWebService, String serviceName, String endpointPathExtra) {
+        EndpointImpl endpoint = new EndpointImpl(springBus, validationWebService);
+        endpoint.setWsdlLocation("wsdl/siva-hashcode-validation.wsdl");
+        endpoint.setServiceName(new QName(SIVA_SERVICE_NAMESPACE, serviceName));
+        endpoint.getOutInterceptors().addAll(commonEndpointOutInterceptors());
+        endpoint.getInInterceptors().add(new SoapRequestHashcodeValidationInterceptor());
+        if (StringUtils.isNotBlank(wsdlConfProperties.getHashcodeValidationWebServiceUrl())) {
+            endpoint.setPublishedEndpointUrl(constructPublishedEndpointUrl(wsdlConfProperties.getHashcodeValidationWebServiceUrl(), HASHCODE_VALIDATION_WEB_SERVICE_ENDPOINT));
+        }
+        endpoint.publish(constructPublishingPath(HASHCODE_VALIDATION_WEB_SERVICE_ENDPOINT, endpointPathExtra));
+        return endpoint;
+    }
+
+    private EndpointImpl constructDataFilesEndpoint(SpringBus springBus, DataFilesWebService validationWebService, String serviceName, String endpointPathExtra) {
+        EndpointImpl endpoint = new EndpointImpl(springBus, validationWebService);
+        endpoint.setWsdlLocation("wsdl/siva-datafiles.wsdl");
+        endpoint.setServiceName(new QName(SIVA_SERVICE_NAMESPACE, serviceName));
+        if (StringUtils.isNotBlank(wsdlConfProperties.getDataFilesWebServiceUrl())) {
+            endpoint.setPublishedEndpointUrl(constructPublishedEndpointUrl(wsdlConfProperties.getDataFilesWebServiceUrl(), DATAFILES_ENDPOINT));
+        }
+        endpoint.publish(constructPublishingPath(DATAFILES_ENDPOINT, endpointPathExtra));
+        return endpoint;
+    }
+
+    private String constructPublishedEndpointUrl(String url, String path) {
+        try {
+            return new URIBuilder(url).setPath("soap" + path).build().toString();
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException("Failed to construct SOAP publication endpoint url from " + url + " and path " + path, e);
+        }
+    }
+
+    private String constructPublishingPath(String endpointPath, String endpointPathExtra) {
+        return endpointPath + (StringUtils.isNotBlank(endpointPathExtra) ? endpointPathExtra : "");
     }
 
     public List<UrlHealthIndicator.ExternalLink> getDefaultExternalLinks() {
@@ -161,7 +217,7 @@ public class ServletConfiguration extends MonitoringConfiguration {
     }
 
     @Autowired
-    public void setWsdlServiceConfigurationProperties(WsdlServiceConfigurationProperties wsdlServiceConfigurationProperties) {
-        this.wsdlServiceConfigurationProperties = wsdlServiceConfigurationProperties;
+    public void setWsdlConfProperties(WsdlServiceConfigurationProperties wsdlConfProperties) {
+        this.wsdlConfProperties = wsdlConfProperties;
     }
 }

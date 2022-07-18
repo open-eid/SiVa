ed38f6d552fb2f79d61e8d42e91850e664904434
Heiti Tobi <Heiti.Tobi@nortal.com>
2022-07-18 10:52:48 +0300

    SIVA-352 Removed xroad related monitoring test, updated xroad tests

--- siva-parent/siva-test/src/test/java/ee/openeid/siva/integrationtest/XRoadValidationFAilIT.java
+++ siva-parent/siva-test/src/test/java/ee/openeid/siva/integrationtest/XRoadValidationFAilIT.java
@@ -14,19 +14,18 @@
  * See the Licence for the specific language governing permissions and limitations under the Licence.
  */
 
-package ee.openeid.siva.xroadtest;
+package ee.openeid.siva.integrationtest;
 
-import ee.openeid.siva.integrationtest.SiVaRestTests;
 import ee.openeid.siva.xroadtest.configuration.XroadIntegrationTest;
 import org.apache.commons.codec.binary.Base64;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import static ee.openeid.siva.integrationtest.TestData.VALIDATION_CONCLUSION_PREFIX;
+import static ee.openeid.siva.integrationtest.TestData.*;
 
 @Category(XroadIntegrationTest.class)
-public class XRoadValidationPassIT extends SiVaRestTests {
+public class XRoadValidationFAilIT extends SiVaRestTests {
 
     @Override
     protected String getTestFilesDirectory() {
@@ -34,7 +33,7 @@ public class XRoadValidationPassIT extends SiVaRestTests {
     }
 
     /**
-     * TestCaseID: Xroad-ValidationPass-1
+     * TestCaseID: Xroad-ValidationFail-1
      *
      * TestType: Automated
      *
@@ -42,22 +41,24 @@ public class XRoadValidationPassIT extends SiVaRestTests {
      *
      * Title: Simple xroad document
      *
-     * Expected Result: Document should pass
+     * Expected Result: Document should fail as xroad document validation is not supported
      *
      * File: xroad-simple.asice
      */
     @Test
-    public void validatingSimpleXroadDocumentShouldReturnAReport() {
+    public void validatingSimpleXroadDocumentShouldFail() {
         String encodedString = Base64.encodeBase64String(readFileFromTestResources("xroad-simple.asice"));
         post(validationRequestWithDocumentTypeValidKeys(encodedString, "xroad-simple.asice", "xroad", VALID_SIGNATURE_POLICY_3))
                 .then().rootPath(VALIDATION_CONCLUSION_PREFIX)
-                .body("signatureForm", Matchers.is("ASiC-E"))
-                .body("signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_LT"))
-                .body("signatures[0].indication", Matchers.is("TOTAL-PASSED"))
+                .body("signatureForm", Matchers.is(SIGNATURE_FORM_ASICE))
+                .body("validationLevel", Matchers.is(VALIDATION_LEVEL_ARCHIVAL_DATA))
+                .body("signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_B"))
+                .body("signatures[0].indication", Matchers.is(TOTAL_FAILED))
+                .body("signatures[0].subIndication", Matchers.is(SUB_INDICATION_FORMAT_FAILURE))
                 .body("signatures[0].subjectDistinguishedName.serialNumber", Matchers.is("70006317"))
                 .body("signatures[0].subjectDistinguishedName.commonName", Matchers.is("Riigi Infosüsteemi Amet"))
-                .body("signatures[0].errors", Matchers.emptyOrNullString())
-                .body("validSignaturesCount", Matchers.is(1))
+                .body("signatures[0].errors.content", Matchers.hasItems(SIG_UNEXPECTED_FORMAT))
+                .body("validSignaturesCount", Matchers.is(0))
                 .body("signaturesCount", Matchers.is(1));
     }
 
@@ -70,22 +71,24 @@ public class XRoadValidationPassIT extends SiVaRestTests {
      *
      * Title: Batchsignature xroad document
      *
-     * Expected Result: Document should pass
+     * Expected Result: Document should fail as xroad document validation is not supported
      *
      * File: xroad-batchsignature.asice
      */
     @Test
-    public void validatingBatchXroadDocumentShouldReturnAReport() {
+    public void validatingBatchXroadDocumentShouldFail() {
         String encodedString = Base64.encodeBase64String(readFileFromTestResources("xroad-batchsignature.asice"));
         post(validationRequestWithDocumentTypeValidKeys(encodedString, "xroad-batchsignature.asice", "xroad", VALID_SIGNATURE_POLICY_3))
                 .then().rootPath(VALIDATION_CONCLUSION_PREFIX)
-                .body("signatureForm", Matchers.is("ASiC-E_batchsignature"))
-                .body("signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_B_BES"))
-                .body("signatures[0].indication", Matchers.is("TOTAL-PASSED"))
-                .body("signatures[0].errors", Matchers.emptyOrNullString())
+                .body("signatureForm", Matchers.is(SIGNATURE_FORM_ASICE))
+                .body("validationLevel", Matchers.is(VALIDATION_LEVEL_ARCHIVAL_DATA))
+                .body("signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_B"))
+                .body("signatures[0].indication", Matchers.is(TOTAL_FAILED))
+                .body("signatures[0].subIndication", Matchers.is(SUB_INDICATION_FORMAT_FAILURE))
                 .body("signatures[0].subjectDistinguishedName.serialNumber", Matchers.is("70006317"))
                 .body("signatures[0].subjectDistinguishedName.commonName", Matchers.is("Riigi Infosüsteemi Amet"))
-                .body("validSignaturesCount", Matchers.is(1))
+                .body("signatures[0].errors.content", Matchers.hasItems(SIG_UNEXPECTED_FORMAT))
+                .body("validSignaturesCount", Matchers.is(0))
                 .body("signaturesCount", Matchers.is(1));
     }
 
@@ -98,22 +101,24 @@ public class XRoadValidationPassIT extends SiVaRestTests {
      *
      * Title: Attachment xroad document
      *
-     * Expected Result: Document should pass
+     * Expected Result: Document should fail as xroad document validation is not supported
      *
      * File: xroad-attachment.asice
      */
     @Test
-    public void validatingAttachXroadDocumentShouldReturnAReport() {
+    public void validatingAttachXroadDocumentShouldFail() {
         String encodedString = Base64.encodeBase64String(readFileFromTestResources("xroad-attachment.asice"));
         post(validationRequestWithDocumentTypeValidKeys(encodedString, "xroad-attachment.asice", "xroad", VALID_SIGNATURE_POLICY_3))
                 .then().rootPath(VALIDATION_CONCLUSION_PREFIX)
-                .body("signatureForm", Matchers.is("ASiC-E_batchsignature"))
-                .body("signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_B_BES"))
-                .body("signatures[0].indication", Matchers.is("TOTAL-PASSED"))
-                .body("signatures[0].errors", Matchers.emptyOrNullString())
+                .body("signatureForm", Matchers.is(SIGNATURE_FORM_ASICE))
+                .body("validationLevel", Matchers.is(VALIDATION_LEVEL_ARCHIVAL_DATA))
+                .body("signatures[0].signatureFormat", Matchers.is("XAdES_BASELINE_B"))
+                .body("signatures[0].indication", Matchers.is(TOTAL_FAILED))
+                .body("signatures[0].subIndication", Matchers.is(SUB_INDICATION_FORMAT_FAILURE))
                 .body("signatures[0].subjectDistinguishedName.serialNumber", Matchers.is("70006317"))
                 .body("signatures[0].subjectDistinguishedName.commonName", Matchers.is("Riigi Infosüsteemi Amet"))
-                .body("validSignaturesCount", Matchers.is(1))
+                .body("signatures[0].errors.content", Matchers.hasItems(SIG_UNEXPECTED_FORMAT))
+                .body("validSignaturesCount", Matchers.is(0))
                 .body("signaturesCount", Matchers.is(1));
     }
 

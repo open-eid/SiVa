67cced1fe11f893b2b7ad7723a75fd37e4d305eb
ivoMattus <ivo.mattus@gmail.com>
2023-07-13 17:26:46 +0300

    SIVA-454 Replace deprecated WebSecurityConfigurerAdapter with SecurityFilterChain

--- siva-parent/siva-webapp/src/main/java/ee/openeid/siva/webapp/configuration/SecurityConfiguration.java
+++ siva-parent/siva-webapp/src/main/java/ee/openeid/siva/webapp/configuration/SecurityConfiguration.java
@@ -1,20 +1,24 @@
 package ee.openeid.siva.webapp.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .mvcMatchers("/monitoring").denyAll()
                 .mvcMatchers("/").permitAll()
                 .and()
                 .csrf().disable();
+
+        return http.build();
     }
 }

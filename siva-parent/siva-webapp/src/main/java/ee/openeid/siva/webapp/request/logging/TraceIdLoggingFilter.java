028503fbf68916c2966bdadb762acc2df2289652
Juku Trump <juku.trump@nortal.com>
2024-04-05 16:21:29 +0300

    SIVA-635 Replace Spring Cloud Sleuth with TraceIdLoggingFilter

---
+++ siva-parent/siva-webapp/src/main/java/ee/openeid/siva/webapp/request/logging/TraceIdLoggingFilter.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 Riigi Infosüsteemi Amet
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+
+package ee.openeid.siva.webapp.request.logging;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE) // Ensure that logging attributes are set as early as possible.
+public class TraceIdLoggingFilter extends OncePerRequestFilter {
+
+    private static final String REQUEST_ATTRIBUTE_NAME_REQUEST_ID = "requestId";
+    private static final String TRACE_ID = "trace.id";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestTraceId = MDC.get(TRACE_ID);
+        boolean traceIdExists = StringUtils.isNotEmpty(requestTraceId);
+        if (!traceIdExists) {
+            // Use same format as Elastic APM Agent.
+            requestTraceId = RandomStringUtils.random(32, "0123456789abcdef");
+        }
+
+        // Set traceId also as HttpServletRequest attribute to make it accessible for Tomcat's AccessLogValve.
+        request.setAttribute(REQUEST_ATTRIBUTE_NAME_REQUEST_ID, requestTraceId);
+
+        if (!traceIdExists) {
+            MDC.put(TRACE_ID, requestTraceId);
+        }
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            if (!traceIdExists) {
+                MDC.remove(TRACE_ID);
+            }
+        }
+    }
+
+}
---
+++ siva-parent/siva-webapp/src/test/java/ee/openeid/siva/webapp/request/logging/TraceIdLoggingFilterTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2024 Riigi Infosüsteemi Amet
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
+
+package ee.openeid.siva.webapp.request.logging;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.MDC;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+class TraceIdLoggingFilterTest {
+
+    @InjectMocks
+    private TraceIdLoggingFilter filter;
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
+    @Mock
+    private HttpServletResponse httpServletResponse;
+    @Mock
+    private FilterChain filterChain;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "abcd012345", " "})
+    void doFilterInternal_WhenGivenTraceIdInMdc_RequestIdIsStoredAsRequestAttribute(String correlationId) throws Exception {
+        try (MDC.MDCCloseable traceIdScope = MDC.putCloseable("trace.id", correlationId)) {
+            filter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+        }
+
+        verify(httpServletRequest).setAttribute("requestId", correlationId);
+        verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+        verifyNoMoreInteractions(httpServletRequest, filterChain);
+        verifyNoInteractions(httpServletResponse);
+    }
+
+    @Test
+    void doFilterInternal_WhenEmptyTraceIdInMdc_RequestIdIsGeneratedAndStoredAsRequestAttribute() throws Exception {
+        try (MDC.MDCCloseable traceIdScope = MDC.putCloseable("trace.id", "")) {
+            filter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+        }
+
+        verify(httpServletRequest).setAttribute(eq("requestId"), anyString());
+        verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+        verifyNoMoreInteractions(httpServletRequest, filterChain);
+        verifyNoInteractions(httpServletResponse);
+    }
+
+    @Test
+    void doFilterInternal_WhenNoCorrelationIdInMdc_RequestIdIsGeneratedAndStoredAsRequestAttribute() throws Exception {
+        filter.doFilterInternal(httpServletRequest, httpServletResponse, filterChain);
+
+        verify(httpServletRequest).setAttribute(eq("requestId"), anyString());
+        verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+        verifyNoMoreInteractions(httpServletRequest, filterChain);
+        verifyNoInteractions(httpServletResponse);
+    }
+
+}
